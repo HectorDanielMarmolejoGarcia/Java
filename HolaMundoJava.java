76c392986440b87a60fc20805ccc3997b3a36c47
Marmolejo García Héctor Daniel <91081737+HectorDanielMarmolejoGarcia@users.noreply.github.com>
2021-10-05 16:04:32 -0500

    Hola mundo en java
    Hola mundo desde vim con el lenguaje java

---
+++ HolaMundoJava.java
@@ -0,0 +1,16 @@
+public class HolaMundoJava {
+	/*
+	 * Aplicacion Java
+	 * Title           :Hola Mundo en Java
+	 * Description     :Hola mundo en java desde vim en windows 
+	 * Author          :Hector Daniel Marmolejo Garcia 
+	 * Date            :20211004
+	 * version         :1
+	 * Usage           :c:\>javac /path/HolaMundoJava.java
+	 *
+	 * Hola mundo en java
+	 */
+	public static void main(String []args){
+		System.out.println("HOLA MUNDO EN JAVA");
+	}
+}
